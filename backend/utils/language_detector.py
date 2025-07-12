8e02f93249defcb3382331013a288bd87261d39d
Anushka Waghmare <anushkawaghmare8965@gmail.com>
2025-07-12 22:36:24 +0530

    Update language_detector.py

--- backend/utils/language_detector.py
+++ backend/utils/language_detector.py
@@ -1,23 +1,27 @@
 from langdetect import detect_langs
 
-def detect_dominant_language(text: str, fallback: str = "en-IN") -> str:
+def fallback_language_detection(text: str, sarvam_lang: str, fallback: str = "en-IN") -> str:
     try:
+        if sarvam_lang != "en-IN":
+            return sarvam_lang
+
         results = detect_langs(text)
         lang_probs = {r.lang: r.prob for r in results}
 
-        if "mr" in lang_probs and lang_probs["mr"] > 0.5:
-            return "mr-IN"
-        elif "hi" in lang_probs and lang_probs["hi"] > 0.5:
+        if "hi" in lang_probs and lang_probs["hi"] > 0.4:
             return "hi-IN"
-        elif "en" in lang_probs and lang_probs["en"] > 0.5:
-            return "en-IN"
-        elif "ta" in lang_probs and lang_probs["ta"] > 0.5:
+        elif "mr" in lang_probs and lang_probs["mr"] > 0.4:
+            return "mr-IN"
+        elif "ta" in lang_probs and lang_probs["ta"] > 0.4:
             return "ta-IN"
-        elif "bn" in lang_probs and lang_probs["bn"] > 0.5:
+        elif "bn" in lang_probs and lang_probs["bn"] > 0.4:
             return "bn-IN"
-        elif "te" in lang_probs and lang_probs["te"] > 0.5:
+        elif "te" in lang_probs and lang_probs["te"] > 0.4:
             return "te-IN"
-        else:
-            return fallback
+        elif "gu" in lang_probs and lang_probs["gu"] > 0.4:
+            return "gu-IN"
+        elif "kn" in lang_probs and lang_probs["kn"] > 0.4:
+            return "kn-IN"
+        return fallback
     except Exception:
         return fallback
